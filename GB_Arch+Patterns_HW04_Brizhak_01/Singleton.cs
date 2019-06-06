4a6e1be3239f16bb37e57a970358e40296d3f766
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-06 20:45:46 +0800

    +class Singleton

--- GB_Arch+Patterns_HW04_Brizhak_01/Singleton.cs
+++ GB_Arch+Patterns_HW04_Brizhak_01/Singleton.cs
@@ -8,6 +8,24 @@ namespace GB_Arch_Patterns_HW04_Brizhak_01
 {
    public class Singleton
     {
+        private static Singleton inst;
+        private static object objLock = new Object();
 
+        private Singleton() { }
+        public static Singleton Inst
+        {
+            get
+            {
+                if (inst == null)
+                {
+                    lock (objLock)
+                    {
+                        if (inst == null)
+                            inst = new Singleton();
+                    }
+                }
+                return inst;
+            }
+        }
     }
 }
