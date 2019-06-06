9b6b1507a733322c37511bd93286bc747ea19970
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-06 20:38:44 +0800

    +

--- GB_Arch+Patterns_HW04_Brizhak_01/Program.cs
+++ GB_Arch+Patterns_HW04_Brizhak_01/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+    //ДЗ к уроку 4 по Курсу "Архитектуры и шаблоны программирования" Брижак Андрей
+    //1. Реализовать шаблон «Одиночка» для многопоточной программы с использованием оператора
+    //lock.
+
 namespace GB_Arch_Patterns_HW04_Brizhak_01
 {
     class Program
