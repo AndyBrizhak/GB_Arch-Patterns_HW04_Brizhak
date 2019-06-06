8a81a3b7993c3d6cbbe86cbbd1c67d81fed80339
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-06 20:01:12 +0800

    Добавьте файлы проекта.

---
+++ GB_Arch+Patterns_HW04_Brizhak_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW04_Brizhak_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
